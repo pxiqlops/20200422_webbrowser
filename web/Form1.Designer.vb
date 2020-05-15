2a6cff80348dbb6a7feb334779634ec8171b35a2
tatsumi mukai <63081975+pxiqlops@users.noreply.github.com>
2020-05-15 17:40:50 +0900

    webブラウザ

---
+++ web/Form1.Designer.vb
@@ -0,0 +1,122 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.WebBrowser1 = New System.Windows.Forms.WebBrowser()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.URL = New System.Windows.Forms.TextBox()
+        Me.sendbt = New System.Windows.Forms.Button()
+        Me.backbt = New System.Windows.Forms.Button()
+        Me.forbt = New System.Windows.Forms.Button()
+        Me.reloadbt = New System.Windows.Forms.Button()
+        Me.homebt = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'WebBrowser1
+        '
+        Me.WebBrowser1.Location = New System.Drawing.Point(49, 63)
+        Me.WebBrowser1.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.WebBrowser1.Name = "WebBrowser1"
+        Me.WebBrowser1.Size = New System.Drawing.Size(1402, 675)
+        Me.WebBrowser1.TabIndex = 0
+        '
+        'URL
+        '
+        Me.URL.Location = New System.Drawing.Point(259, 19)
+        Me.URL.Name = "URL"
+        Me.URL.Size = New System.Drawing.Size(1145, 22)
+        Me.URL.TabIndex = 2
+        '
+        'sendbt
+        '
+        Me.sendbt.Location = New System.Drawing.Point(1410, 19)
+        Me.sendbt.Name = "sendbt"
+        Me.sendbt.Size = New System.Drawing.Size(41, 23)
+        Me.sendbt.TabIndex = 3
+        Me.sendbt.Text = "GO"
+        Me.sendbt.UseVisualStyleBackColor = True
+        '
+        'backbt
+        '
+        Me.backbt.Location = New System.Drawing.Point(49, 17)
+        Me.backbt.Name = "backbt"
+        Me.backbt.Size = New System.Drawing.Size(50, 25)
+        Me.backbt.TabIndex = 4
+        Me.backbt.Text = "戻る"
+        Me.backbt.UseVisualStyleBackColor = True
+        '
+        'forbt
+        '
+        Me.forbt.Location = New System.Drawing.Point(105, 17)
+        Me.forbt.Name = "forbt"
+        Me.forbt.Size = New System.Drawing.Size(50, 25)
+        Me.forbt.TabIndex = 5
+        Me.forbt.Text = "進む"
+        Me.forbt.UseVisualStyleBackColor = True
+        '
+        'reloadbt
+        '
+        Me.reloadbt.Location = New System.Drawing.Point(161, 17)
+        Me.reloadbt.Name = "reloadbt"
+        Me.reloadbt.Size = New System.Drawing.Size(50, 25)
+        Me.reloadbt.TabIndex = 6
+        Me.reloadbt.Text = "更新"
+        Me.reloadbt.UseVisualStyleBackColor = True
+        '
+        'homebt
+        '
+        Me.homebt.Location = New System.Drawing.Point(217, 17)
+        Me.homebt.Name = "homebt"
+        Me.homebt.Size = New System.Drawing.Size(36, 25)
+        Me.homebt.TabIndex = 7
+        Me.homebt.Text = "🏡"
+        Me.homebt.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1505, 750)
+        Me.Controls.Add(Me.homebt)
+        Me.Controls.Add(Me.reloadbt)
+        Me.Controls.Add(Me.forbt)
+        Me.Controls.Add(Me.backbt)
+        Me.Controls.Add(Me.sendbt)
+        Me.Controls.Add(Me.URL)
+        Me.Controls.Add(Me.WebBrowser1)
+        Me.Name = "Form1"
+        Me.Text = "WebBrowser"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents WebBrowser1 As WebBrowser
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents URL As TextBox
+    Friend WithEvents sendbt As Button
+    Friend WithEvents backbt As Button
+    Friend WithEvents forbt As Button
+    Friend WithEvents reloadbt As Button
+    Friend WithEvents homebt As Button
+End Class
---
+++ web/Form1.vb
@@ -0,0 +1,40 @@
+﻿Public Class Form1
+    Private buttonNo As Integer
+    Private myURL As Uri
+    Dim strURL As String
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        '初期表示としてGoogleのトップページを表示
+        WebBrowser1.Navigate("https://www.google.com/")
+    End Sub
+    Private Sub WebBrowser1_DocumentCompleted(ByVal sender As Object, ByVal e As System.Windows.Forms.
+                                              WebBrowserDocumentCompletedEventArgs) Handles WebBrowser1.DocumentCompleted
+        'ページ読み込み完了時にアドレスバーにURLを表示させる
+        URL.Text = WebBrowser1.Url.ToString()
+    End Sub
+    Private Sub sendbt_Click(sender As Object, e As EventArgs) Handles sendbt.Click
+        'アドレスバーにURLを入力後、GOボタンでページ遷移処理
+        WebBrowser1.Navigate(URL.Text)
+    End Sub
+    Private Sub URL_KeyPress(sender As Object, e As KeyPressEventArgs) Handles URL.KeyPress
+        'アドレスバーでEnterキー押下時にもページ遷移処理
+        If e.KeyChar = Chr(13) Then
+            WebBrowser1.Navigate(URL.Text)
+        End If
+    End Sub
+    Private Sub backbt_Click(sender As Object, e As EventArgs) Handles backbt.Click
+        '戻るボタン押下時の処理
+        WebBrowser1.GoBack()
+    End Sub
+    Private Sub forbt_Click(sender As Object, e As EventArgs) Handles forbt.Click
+        '進むボタン押下時の処理
+        WebBrowser1.GoForward()
+    End Sub
+    Private Sub reloadbt_Click(sender As Object, e As EventArgs) Handles reloadbt.Click
+        '更新（リロード）ボタン押下時の処理
+        WebBrowser1.Refresh()
+    End Sub
+    Private Sub homebt_Click(sender As Object, e As EventArgs) Handles homebt.Click
+        '家マーク（HOME）ボタン押下時の処理、Googleのトップページを表示
+        WebBrowser1.Navigate("https://www.google.com/")
+    End Sub
+End Class
